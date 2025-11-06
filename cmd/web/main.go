4127a1ecb30f298ebf58cedd129b529c393d892f
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-11-06 21:32:29 +0530

    feat: custom logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,39 +2,40 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
-
-type config struct{
-	addr string
+type config struct {
+	addr      string
 	staticDir string
-
 }
 
 func main() {
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "staticDir", "./ui/static/", "Path to static assests")
+	flag.Parse()
 
-	var cfg config 
+	// Use the slog.New() function to initialize a new structured logger, which
+	// writes to the standard out stream and uses the default settings
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
 	// Use the http.NewServeMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
-	
-	flag.StringVar(&cfg.addr, "addr",":4000","HTTP network address")
-	flag.StringVar(&cfg.staticDir,"staticDir","./ui/static/","Path to static assests")
-	flag.Parse()
 
-	fileServer:=http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
-	mux.Handle("GET /static/", http.StripPrefix("/static",fileServer))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /glyst/view/{id}", glystView)
 	mux.HandleFunc("GET /glyst/create", glystCreate)
 	mux.HandleFunc("POST /glyst/create", glystCreatePost)
 
 	// Print a log message to say that the server is starting.
-	log.Print("starting server on:", cfg.addr )
+	logger.Info("starting server","addr" ,cfg.addr)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
@@ -42,5 +43,6 @@ func main() {
 	// we use the log.Fatal() function to log the error message and exit. Note
 	// that any error returned by http.ListenAndServe() is always non-nil.
 	err := http.ListenAndServe(cfg.addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
