4af02f1ac0be016d59675d4968e6dd5ccdc24e12
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-11-07 21:30:16 +0530

    feat: isolate app routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,15 +11,8 @@ type application struct {
 	logger *slog.Logger
 }
 
-type config struct {
-	addr      string
-	staticDir string
-}
-
 func main() {
-	var cfg config
-	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
-	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static/", "Path to static assests")
+	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
 	// Use the slog.New() function to initialize a new structured logger, which
@@ -35,29 +28,17 @@ func main() {
 		logger: logger,
 	}
 
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /glyst/view/{id}", app.glystView)
-	mux.HandleFunc("GET /glyst/create", app.glystCreate)
-	mux.HandleFunc("POST /glyst/create", app.glystCreatePost)
-
 	// Print a log message to say that the server is starting.
-	logger.Info("starting server", "addr", cfg.addr)
+	logger.Info("starting server", "addr", *addr)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
 	// and the servemux we just created. If http.ListenAndServe() returns an error
 	// we use the log.Fatal() function to log the error message and exit. Note
 	// that any error returned by http.ListenAndServe() is always non-nil.
-	err := http.ListenAndServe(cfg.addr, mux)
-	if err!=nil{
-		logger.Error("Server error","err",err.Error())
+	err := http.ListenAndServe(*addr, app.routes())
+	if err != nil {
+		logger.Error("Server error", "err", err.Error())
 		os.Exit(1)
 	}
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,19 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+
+	// Use the http.NewServeMux() function to initialize a new servemux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /glyst/view/{id}", app.glystView)
+	mux.HandleFunc("GET /glyst/create", app.glystCreate)
+	mux.HandleFunc("POST /glyst/create", app.glystCreatePost)
+	return mux
+}
