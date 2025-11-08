814ff08aee1062318fa5afffa582cbe61e746643
Swaroop-Acharya <swaroopacharya935@gmail.com>
2025-11-08 18:57:22 +0530

    feat: mysql config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	logger *slog.Logger
 }
 
+
+func openDB(dsn string) (*sql.DB, error){
+	db, err:= sql.Open("mysql", dsn)
+	
+	if err!=nil{
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err !=nil{
+		db.Close()
+		return nil, err
+
+	}
+	return db, err
+}
+
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "web:pass@tcp(localhost:3306)/glyst?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	// Use the slog.New() function to initialize a new structured logger, which
@@ -21,6 +42,17 @@ func main() {
 		Level:     slog.LevelDebug,
 		AddSource: true,
 	}))
+
+	
+	db,err:=openDB(*dsn)
+	if err!=nil{
+		logger.Error(err.Error())
+		os.Exit(1)	
+	}
+
+
+	defer db.Close()
+
 
 	// Initialize a new instance of our application struct, containing the
 	// dependencies (for now, just the structured logger).
@@ -36,7 +68,7 @@ func main() {
 	// and the servemux we just created. If http.ListenAndServe() returns an error
 	// we use the log.Fatal() function to log the error message and exit. Note
 	// that any error returned by http.ListenAndServe() is always non-nil.
-	err := http.ListenAndServe(*addr, app.routes())
+	err = http.ListenAndServe(*addr, app.routes())
 	if err != nil {
 		logger.Error("Server error", "err", err.Error())
 		os.Exit(1)
